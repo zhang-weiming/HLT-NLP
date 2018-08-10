db1960d39852790e2bbfbaa259a9471e8a2136e5
zwm <1210500550@qq.com>
2018-08-10 22:46:38 +0800

    修改错误：计算Q E时数据使用格式不对

--- HMM/hmm.py
+++ HMM/hmm.py
@@ -22,26 +22,36 @@ def hmm(words_with_tag, tags, V, S):
     for tag in S:
         if tag not in count_tag:
             count_tag[tag] = count(tag, tags)
-        a = count("*" + tag, tags) + Alpha
-        b = count_tag["*"] + Alpha * len_V
-        Q[tag + "*"] = a / b
+        a, b = count("* " + tag, tags) + Alpha, count_tag["*"] + Alpha * len_V
+        Q[tag + "|*"] = a / b
+        a, b = count(tag + " STOP", tags) + Alpha, count_tag[tag] + Alpha * len_V
+        Q["STOP|" + tag] = a / b
 
         for prefix in S:
             if prefix not in count_tag:
                 count_tag[prefix] = count(prefix, tags)
-            a = count(prefix + tag, tags) + Alpha
+            a = count(prefix + " " + tag, tags) + Alpha
             b = count_tag[prefix] + Alpha * len_V
-            Q[tag + prefix] = a / b
+            Q["%s|%s" % (tag, prefix)] = a / b
     for word in V:
         for tag in S:
             a = words_with_tag.count("%s %s" % (word, tag)) + Alpha
             b = count_tag[tag] + Alpha * len_V
-            E["%s %s" % (word, tag)] = a / b
+            E["%s|%s" % (word, tag)] = a / b
 
     return Q, E
 
 
 def train():
+    """
+    words_with_tag：
+        维度：1 x n
+        含义：所有出现过的词，与其词性的组合，不去重。组合形式如：“戴相龙|NR”。
+    tags：
+        维度：n x m
+        含义：将每条句子中词的词性按出现顺序连接，用空格作连接符。每个词条的句首和句尾分别添加“*”、“STOP”
+    :return:
+    """
     print("train...", end="")
     sys.stdout.flush()
     with codecs.open("data/train.conll", "r", "utf-8") as fr:
@@ -49,14 +59,14 @@ def train():
         words_with_tag, tags, V, S, tags_tmp = [], [], set(), set(), "*"
         for line in data:
             if line.strip() == "":
-                tags.append(tags_tmp[:-1])
+                tags.append(tags_tmp + " STOP")
                 tags_tmp = "*"
             else:
                 s = line.split("\t")
-                words_with_tag.append("%s %s" % (s[1], s[3]))
+                words_with_tag.append("%s|%s" % (s[1], s[3]))
                 V.add(s[1])
                 S.add(s[3])
-                tags_tmp += s[3] + " "
+                tags_tmp += " " + s[3]
 
     Q, E = hmm(words_with_tag, tags, V, S)
 
@@ -75,25 +85,26 @@ def load_model():
 
 
 def main():
-    # Q, E = train()
-    Q, E = load_model()
+    Q, E = train()
+    # Q, E = load_model()
     P = []
     with codecs.open("data/train.conll", "r", "utf-8") as fr:
         data = fr.readlines()
-        words_with_tag, tags, words_with_tag_tmp, tags_tmp = [], [], [], ["*"]
+        words_with_tag_tmp, tags_tmp = [], ["*"]
         for line in data:
             if line.strip() == "":
                 p = 1
+                tags_tmp.append("STOP")
+                for i in range(len(tags_tmp)-1):
+                    p *= Q["%s|%s" % (tags_tmp[i+1], tags_tmp[i])]
                 for word_with_tag in words_with_tag_tmp:
                     p *= E[word_with_tag]
-                for i in range(len(tags_tmp)-1):
-                    p *= Q[tags_tmp[i+1] + tags_tmp[i+1]]
-                print(p)
+                print(p, " ".join(words_with_tag_tmp))
                 P.append(p)
-                tags_tmp = ["*"]
+                words_with_tag_tmp, tags_tmp = [], ["*"]
             else:
                 s = line.split("\t")
-                words_with_tag_tmp.append("%s %s" % (s[1], s[3]))
+                words_with_tag_tmp.append("%s|%s" % (s[1], s[3]))
                 tags_tmp.append(s[3])
     average = sum(P) / len(P)
     var = sum([(x - average)**2 for x in P]) / len(P)
