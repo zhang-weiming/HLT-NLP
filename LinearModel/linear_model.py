d0d4c0e0f8964af9f8425affe035cfbd52a02a25
zwm <1210500550@qq.com>
2018-08-25 16:52:27 +0800

    调整get_f函数，把所有特征从if语句分离，保存在变量中

--- LinearModel/linear_model.py
+++ LinearModel/linear_model.py
@@ -1,5 +1,15 @@
+"""
+0.7977070853129519
+0.8114645734352407
+0.8400950216897335
+0.85449287337327
+0.8517041933484817
+
+68.11372590065002
+"""
 import codecs
 import numpy as np
+import time
 
 
 class LinearModel(object):
@@ -52,34 +62,42 @@ class LinearModel(object):
 
     def get_f(self, sent, i):
         f = []
-        if "02: %s" % sent[i][0] in self.features:
-            f.append(self.features["02: %s" % sent[i][0]])
-        if "03: %s" % sent[i-1][0] in self.features:
-            f.append(self.features["03: %s" % sent[i-1][0]])
-        if "04: %s" % sent[i+1][0] in self.features:
-            f.append(self.features["04: %s" % sent[i+1][0]])
-        if "05: %s %s" % (sent[i][0], sent[i-1][0][-1]) in self.features:
-            f.append(self.features["05: %s %s" % (sent[i][0], sent[i-1][0][-1])])
-        if "06: %s %s" % (sent[i][0], sent[i+1][0][0]) in self.features:
-            f.append(self.features["06: %s %s" % (sent[i][0], sent[i+1][0][0])])
-        if "07: %s" % sent[i][0][0] in self.features:
-            f.append(self.features["07: %s" % sent[i][0][0]])
-        if "08: %s" % sent[i][0][-1] in self.features:
-            f.append(self.features["08: %s" % sent[i][0][-1]])
-        if "09: %s" % sent[i][0][0:-2] in self.features:
-            f.append(self.features["09: %s" % sent[i][0][0:-2]])
-        if "10: %s %s" % (sent[i][0][0], sent[i][0][0:-2]) in self.features:
-            f.append(self.features["10: %s %s" % (sent[i][0][0], sent[i][0][0:-2])])
-        if "11: %s %s" % (sent[i][0][-1], sent[i][0][0:-2]) in self.features:
-            f.append(self.features["11: %s %s" % (sent[i][0][-1], sent[i][0][0:-2])])
-        if len(sent[i][0]) == 1 and "12: %s %s %s" % (sent[i][0], sent[i-1][0][-1],sent[i+1][0][0]) in self.features:
-            f.append(self.features["12: %s %s %s" % (sent[i][0], sent[i-1][0][-1],sent[i+1][0][0])])
-        if sent[i][0][0:-2] == sent[i][0][1:-1] and "13: %s consecutive" % sent[i][0][0:-2] in self.features:
-            f.append(self.features["13: %s consecutive" % sent[i][0][0:-2]])
-        if "14: %s" % sent[i][0][0:4] in self.features:
-            f.append(self.features["14: %s" % sent[i][0][0:4]])
-        if "15: %s" % sent[i][0][len(sent[i][0])-4:len(sent[i][0])] in self.features:
-            f.append(self.features["15: %s" % sent[i][0][len(sent[i][0])-4:len(sent[i][0])]])
+        f_02, f_03, f_04 = "02: %s" % sent[i][0], "03: %s" % sent[i-1][0], "04: %s" % sent[i+1][0]
+        f_05, f_06 = "05: %s %s" % (sent[i][0], sent[i-1][0][-1]), "06: %s %s" % (sent[i][0], sent[i+1][0][0])
+        f_07, f_08, f_09 = "07: %s" % sent[i][0][0], "08: %s" % sent[i][0][-1], "09: %s" % sent[i][0][0:-2]
+        f_10, f_11 = "10: %s %s" % (sent[i][0][0], sent[i][0][0:-2]), "11: %s %s" % (sent[i][0][-1], sent[i][0][0:-2])
+        f_12 = "12: %s %s %s" % (sent[i][0], sent[i-1][0][-1],sent[i+1][0][0])
+        f_13 = "13: %s consecutive" % sent[i][0][0:-2]
+        f_14, f_15 = "14: %s" % sent[i][0][0:4], "15: %s" % sent[i][0][len(sent[i][0])-4:len(sent[i][0])]
+
+        if f_02 in self.features:
+            f.append(self.features[f_02])
+        if f_03 in self.features:
+            f.append(self.features[f_03])
+        if f_04 in self.features:
+            f.append(self.features[f_04])
+        if f_05 in self.features:
+            f.append(self.features[f_05])
+        if f_06 in self.features:
+            f.append(self.features[f_06])
+        if f_07 in self.features:
+            f.append(self.features[f_07])
+        if f_08 in self.features:
+            f.append(self.features[f_08])
+        if f_09 in self.features:
+            f.append(self.features[f_09])
+        if f_10 in self.features:
+            f.append(self.features[f_10])
+        if f_11 in self.features:
+            f.append(self.features[f_11])
+        if len(sent[i][0]) == 1 and f_12 in self.features:
+            f.append(self.features[f_12])
+        if sent[i][0][0:-2] == sent[i][0][1:-1] and f_13 in self.features:
+            f.append(self.features[f_13])
+        if f_14 in self.features:
+            f.append(self.features[f_14])
+        if f_15 in self.features:
+            f.append(self.features[f_15])
         return f
 
     def get_argmax(self, sent, i):
@@ -94,7 +112,8 @@ class LinearModel(object):
         return maxtag, f
 
     def train(self):
-        M = 10
+        M = 5
+        t1 = time.time()
         for m in range(M):
             for sent in self.train_data:
                 for i in range(1, len(sent) - 1):
@@ -108,6 +127,8 @@ class LinearModel(object):
             # print(sum(self.w[self.tags["NN"]]))
             # 评价一次
             print(self.evaluate())
+        t2 = time.time()
+        print("time cost:", t2 - t1)
 
     def evaluate(self):
         p, n = 0, 0
